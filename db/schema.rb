a520c77ad58c49db1c851f21fd03f26dc614809f
Ville Vainio <ville.vainio@gmail.com>
2015-01-12 17:17:12 +0200

    Add schema for beers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112145608) do
+ActiveRecord::Schema.define(version: 20150112151350) do
+
+  create_table "beers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "style"
+    t.integer  "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "breweries", force: :cascade do |t|
     t.string   "name"
