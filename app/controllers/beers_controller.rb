cb78ea4d3c3d6f9dd5f65f38f4496a615f14c153
Ville Vainio <ville.vainio@gmail.com>
2015-01-25 15:09:44 +0200

    Redirect to beers#index after beer creation

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -29,7 +29,7 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to @beer, notice: 'Beer was successfully created.' }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
         format.json { render :show, status: :created, location: @beer }
       else
         format.html { render :new }
