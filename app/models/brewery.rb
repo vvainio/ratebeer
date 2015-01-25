f99e76c2d5905d440c0c0509cfc34c1b708f5064
Ville Vainio <ville.vainio@gmail.com>
2015-01-25 17:02:24 +0200

    Add average rating for breweries and render to breweries#show

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,8 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  def average_rating
+    ratings.average(:score).round(1)
+  end
 end
