f06db9be06d12853ef60b8c1552e4524ff9d25ce
Ville Vainio <ville.vainio@gmail.com>
2015-01-25 15:24:14 +0200

    Set beer styles to be predefined & add select to form

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -16,10 +16,13 @@ class BeersController < ApplicationController
   def new
     @beer = Beer.new
     @breweries = Brewery.all
+    @styles = ['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
   end
 
   # GET /beers/1/edit
   def edit
+    @breweries = Brewery.all
+    @styles = ['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
   end
 
   # POST /beers
