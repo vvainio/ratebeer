e4533fe40cb2006b940336fe1a6ade2063fcc52c
Ville Vainio <ville.vainio@gmail.com>
2015-01-25 12:31:41 +0200

    Add routes for creating new rating

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   resources :breweries
 
-  get 'ratings', to: 'ratings#index'
+  get 'ratings',     to: 'ratings#index'
+  get 'ratings/new', to: 'ratings#new'
+  post 'ratings',    to: 'ratings#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
