fe9802a114673ee70d8a47256a3ff343d216a191
Ville Vainio <ville.vainio@gmail.com>
2015-01-31 15:22:03 +0200

    Add signout route

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :delete]
 
-  get 'signin', to: 'sessions#new'
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
