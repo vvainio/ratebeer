9c27e5588acac8b4d8a86eec8dea3a16f08ba52e
Ville Vainio <ville.vainio@gmail.com>
2015-01-31 14:09:46 +0200

    Save latest rating to session

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,7 +9,8 @@ class RatingsController < ApplicationController
   end
 
   def create
-    Rating.create params.require(:rating).permit(:score, :beer_id)
+    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+    session[:last_rating] = "#{rating.beer.name} (#{rating.score} points)"
     redirect_to ratings_path
   end
 
