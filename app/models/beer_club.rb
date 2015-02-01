3aec4af65f6e91e2ea45190733377b835a20bf0f
Ville Vainio <ville.vainio@gmail.com>
2015-02-01 16:32:33 +0200

    Add validations for BeerClub

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -2,6 +2,12 @@ class BeerClub < ActiveRecord::Base
   has_many :memberships
   has_many :users, through: :memberships
 
+  validates :name, presence: true
+  validates :founded, numericality: { greater_than_or_equal_to: 1042,
+                                      less_than_or_equal_to: 2015,
+                                      only_integer: true }
+  validates :city, presence: true
+
   def to_s
     "#{name} (#{city} - #{founded})"
   end
