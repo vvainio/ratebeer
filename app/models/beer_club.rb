f2880f69713eada61da868f3ed6d73a054e2430a
Ville Vainio <ville.vainio@gmail.com>
2015-02-01 14:40:15 +0200

    Link users and beer_clubs through memberships

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -1,2 +1,4 @@
 class BeerClub < ActiveRecord::Base
+  has_many :memberships
+  has_many :users, through: :memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
   has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
+  has_many :memberships, dependent: :destroy
+  has_many :beer_clubs, through: :memberships
 
   validates :username, uniqueness: true,
                        length: { minimum: 3,
