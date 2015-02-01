c68de6f0deaff7e8ec7a7f018c66d629f90ad829
Ville Vainio <ville.vainio@gmail.com>
2015-02-01 18:11:36 +0200

    Remove dependent memberships when destroying a club

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -1,5 +1,5 @@
 class BeerClub < ActiveRecord::Base
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
   validates :name, presence: true
