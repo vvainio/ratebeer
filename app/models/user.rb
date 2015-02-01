80dc398f91dc25b7a4d1e0fbdb15fc4718ed7c39
Ville Vainio <ville.vainio@gmail.com>
2015-02-01 11:09:39 +0200

    Link beers and users through ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   include RatingAverage
 
+  has_many :ratings
+  has_many :beers, through: :ratings
+
   validates :username, uniqueness: true,
                        length: { minimum: 3,
                                  maximum: 15 }
-
-  has_many :ratings
 end
