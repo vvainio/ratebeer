1079ec4bfd7f223b85be9dcf061823f8053891f1
Ville Vainio <ville.vainio@gmail.com>
2015-02-04 17:23:49 +0200

    Add before_action for setting breweries & styles

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,6 @@
 class BeersController < ApplicationController
   before_action :set_beer, only: [:show, :edit, :update, :destroy]
+  before_action :set_breweries_and_styles_for_template, only: [:new, :edit, :create, :update]
 
   # GET /beers
   # GET /beers.json
@@ -15,22 +16,16 @@ class BeersController < ApplicationController
   # GET /beers/new
   def new
     @beer = Beer.new
-    @breweries = Brewery.all
-    @styles = ['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
   end
 
   # GET /beers/1/edit
   def edit
-    @breweries = Brewery.all
-    @styles = ['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
   end
 
   # POST /beers
   # POST /beers.json
   def create
     @beer = Beer.new(beer_params)
-    @breweries = Brewery.all
-    @styles = ['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
 
     respond_to do |format|
       if @beer.save
@@ -68,6 +63,7 @@ class BeersController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_beer
       @beer = Beer.find(params[:id])
@@ -76,5 +72,10 @@ class BeersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def beer_params
       params.require(:beer).permit(:name, :style, :brewery_id)
+    end
+
+    def set_breweries_and_styles_for_template
+      @breweries = Brewery.all
+      @styles = ['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
     end
 end
