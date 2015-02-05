74c0fab39b7e1a7a857925cdb5be729f26346000
Ville Vainio <ville.vainio@gmail.com>
2015-02-05 19:17:33 +0200

    Prompt user to sign in when creating a rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -11,7 +11,9 @@ class RatingsController < ApplicationController
   def create
     @rating = Rating.new params.require(:rating).permit(:score, :beer_id)
 
-    if @rating.save
+    if current_user.nil?
+      redirect_to signin_path, notice: 'You need to be signed in.'
+    elsif @rating.save
       current_user.ratings << @rating
       redirect_to user_path current_user
     else
