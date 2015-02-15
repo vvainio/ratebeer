e9dcdca36c9fa16b2e082098f78935794622cdbe
Ville Vainio <ville.vainio@gmail.com>
2015-02-15 12:11:01 +0200

    Modify Beermapping API to use cache and fix specs

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,14 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city) { fetch_places_in(city) }
+  end
+
+  private
+
+  def self.fetch_places_in(city)
+    # url = "http://beermapping.com/webservice/loccity/#{apikey}/"
+
     url = 'http://stark-oasis-9187.herokuapp.com/api/'
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
@@ -11,5 +20,9 @@ class BeermappingApi
     places.inject([]) do | set, place |
       set << Place.new(place)
     end
+  end
+
+  def self.apikey
+    # apikey = ''
   end
 end
--- spec/lib/beermapping_api_spec.rb
+++ spec/lib/beermapping_api_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'BeermappingApi' do
+  before :each do
+    Rails.cache.clear
+  end
+
   it 'When HTTP GET returns zero entries, result is parsed and returned' do
     canned_answer = <<-END_OF_STRING
     <?xml version="1.0" encoding="UTF-8"?><bmp-locations><location><id nil="true"/><name nil="true"/><status nil="true"/><reviewlink nil="true"/><proxylink nil="true"/><blogmap nil="true"/><street nil="true"/><city nil="true"/><state nil="true"/><zip nil="true"/><country nil="true"/><phone nil="true"/><overall nil="true"/><imagecount nil="true"/></location></bmp-locations>
@@ -49,5 +53,48 @@ describe 'BeermappingApi' do
     place2 = places.last
     expect(place2.name).to eq('Belge')
     expect(place2.street).to eq('Kluuvikatu 5')
+  end
+
+  describe 'in case of cache miss' do
+    before :each do
+      Rails.cache.clear
+    end
+
+    it 'When HTTP GET returns one entry, it is parsed and returned' do
+      canned_answer = <<-END_OF_STRING
+      <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>13307</id><name>O'Connell's Irish Bar</name><status>Beer Bar</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=13307</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=13307&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=13307&amp;d=1&amp;type=norm</blogmap><street>Rautatienkatu 24</street><city>Tampere</city><state></state><zip>33100</zip><country>Finland</country><phone>35832227032</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
+      END_OF_STRING
+
+      stub_request(:get, /.*tampere/)
+        .to_return(body: canned_answer, headers: { 'Content-Type': 'text/xml' })
+
+      places = BeermappingApi.places_in('tampere')
+
+      expect(places.size).to eq(1)
+      place = places.first
+      expect(place.name).to eq("O'Connell's Irish Bar")
+      expect(place.street).to eq('Rautatienkatu 24')
+    end
+  end
+
+  describe 'in case of cache hit' do
+    it 'When one entry in cache, it is returned' do
+      canned_answer = <<-END_OF_STRING
+      <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>13307</id><name>O'Connell's Irish Bar</name><status>Beer Bar</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=13307</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=13307&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=13307&amp;d=1&amp;type=norm</blogmap><street>Rautatienkatu 24</street><city>Tampere</city><state></state><zip>33100</zip><country>Finland</country><phone>35832227032</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
+      END_OF_STRING
+
+      stub_request(:get, /.*tampere/)
+        .to_return(body: canned_answer, headers: { 'Content-Type': 'text/xml' })
+
+      # Ensure that data is found in cache
+      BeermappingApi.places_in('tampere')
+
+      places = BeermappingApi.places_in('tampere')
+
+      expect(places.size).to eq(1)
+      place = places.first
+      expect(place.name).to eq("O'Connell's Irish Bar")
+      expect(place.street).to eq('Rautatienkatu 24')
+    end
   end
 end
