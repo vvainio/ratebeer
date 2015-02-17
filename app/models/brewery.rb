c8a92a61dace8e93ae61688161c6f4b5567c92e0
Ville Vainio <ville.vainio@gmail.com>
2015-02-17 19:26:18 +0200

    Define scopes for active & retired breweries

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,6 +4,9 @@ class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  scope :active, -> { where active: true }
+  scope :retired, -> { where active: [nil, false] }
+
   validates :name, presence: true
   validates :year, numericality: { greater_than_or_equal_to: 1042,
                                    less_than_or_equal_to: proc { Time.now.year },
