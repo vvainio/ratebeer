3b82108ebf191fac0976375537ecca69589b3c2c
Ville Vainio <ville.vainio@gmail.com>
2015-02-20 21:25:18 +0200

    Add helper methods for recognizing admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :admin_user
 
   def current_user
     session[:user_id].nil? ? nil : User.find(session[:user_id])
   end
 
+  def admin_user
+    return nil if session[:user_id].nil?
+    User.find(session[:user_id]).admin?
+  end
+
   def ensure_that_signed_in
     redirect_to signin_path, notice: 'You need to sign in first' unless current_user
+  end
+
+  def ensure_that_admin_user
+    redirect_to signin_path, notice: 'This action requires administrator privileges' unless admin_user
   end
 end
