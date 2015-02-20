d9c062bbac482ede73876e2cb913cbaf683e5433
Ville Vainio <ville.vainio@gmail.com>
2015-02-20 21:38:57 +0200

    Add account status (disabled) to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220174758) do
+ActiveRecord::Schema.define(version: 20150220193323) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20150220174758) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.boolean  "admin"
+    t.boolean  "disabled"
   end
 
 end
