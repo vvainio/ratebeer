8d054ee9b28188936ef10a696ade9a13f6195be2
Ville Vainio <ville.vainio@gmail.com>
2015-02-20 22:24:51 +0200

    Deny signing in if user is disabled

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by username: params[:username]
-    if user && user.authenticate(params[:password])
+
+    if user && user.disabled?
+      redirect_to :back, notice: 'Your account is disabled, please contact an administrator.'
+    elsif user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back, #{user.username}!"
     else
