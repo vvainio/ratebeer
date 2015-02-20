788edba7cb49ec9f65b28183e154189dd7338c4d
Ville Vainio <ville.vainio@gmail.com>
2015-02-20 18:25:21 +0200

    Fix feature specs for breweries

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -12,17 +12,22 @@ describe 'Breweries page' do
       @breweries = %w(Koff Karjala Schlenkerla)
       year = 1896
       @breweries.each do |brewery_name|
-        FactoryGirl.create(:brewery, name: brewery_name, year: year += 1)
+        FactoryGirl.create(:brewery, name: brewery_name, year: year += 1, active: true)
       end
 
       visit breweries_path
     end
 
-    it 'lists the breweries and their total number' do
-      expect(page).to have_content "Number of breweries: #{@breweries.count}"
+    it 'lists the active breweries and their total number' do
+      expect(page).to have_content "Number of active breweries: #{@breweries.count}"
       @breweries.each do |brewery_name|
         expect(page).to have_content brewery_name
       end
+    end
+
+    it 'lists the retired breweries and their total number' do
+      FactoryGirl.create(:brewery, name: 'Inactive brewery', year: 1900, active: false)
+      expect(page).to have_content 'Number of retired breweries: 1'
     end
 
     it 'allows user to navigate to page of a Brewery' do
