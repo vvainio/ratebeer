97b3e7144db6e8567c794cfd710e069ef667d1bd
Ville Vainio <ville.vainio@gmail.com>
2015-02-21 15:03:06 +0200

    Render top styles to ratings#index

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -6,6 +6,7 @@ class RatingsController < ApplicationController
     @top_breweries = Brewery.top 3
     @top_beers = Beer.top 3
     @top_raters = User.top 3
+    @top_styles = Style.top 3
     @recent_ratings = Rating.recent
   end
 
