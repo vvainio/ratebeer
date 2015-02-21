2d5574c1708487673e500df655378e544af7c701
Ville Vainio <ville.vainio@gmail.com>
2015-02-21 15:02:53 +0200

    Add top(n) method for Style

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,8 +1,19 @@
 class Style < ActiveRecord::Base
   has_many :beers
+  has_many :ratings, through: :beers
 
   validates :name, presence: true
   validates :description, presence: true
+
+  def self.top(n)
+    Style.all.sort_by { |s| -(s.ratings.average(:score) || 0) }.take(n)
+  end
+
+  # TODO: Figure out why undefined method `average_rating' is thrown unless
+  # the method is defined here
+  def average_rating
+    ratings.average(:score).round(1)
+  end
 
   def to_s
     "#{name}"
