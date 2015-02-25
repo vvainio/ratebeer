6c16b53e67f49b53b77472e948752d853bcc0e0c
Ville Vainio <ville.vainio@gmail.com>
2015-02-25 18:54:44 +0200

    Enable sorting beers by name, brewery & style

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -8,6 +8,14 @@ class BeersController < ApplicationController
   # GET /beers.json
   def index
     @beers = Beer.all
+
+    order = params[:order] || 'name'
+
+    @beers = case order
+             when 'name' then @beers.sort_by(&:name)
+             when 'brewery' then @beers.sort_by { |b| b.brewery.name }
+             when 'style' then @beers.sort_by { |b| b.style.name }
+    end
   end
 
   # GET /beers/1
