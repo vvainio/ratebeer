146d4afa27a050ee4736722ed3c7fe9c1fa9a68f
Ville Vainio <ville.vainio@gmail.com>
2015-03-01 17:53:39 +0200

    Add index for usernames

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301124748) do
+ActiveRecord::Schema.define(version: 20150301155256) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -71,5 +71,7 @@ ActiveRecord::Schema.define(version: 20150301124748) do
     t.boolean  "admin"
     t.boolean  "disabled"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
