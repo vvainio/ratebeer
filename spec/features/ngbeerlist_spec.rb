5e13f312b5072aafaa02353c59d6a23a06be8a1a
Ville Vainio <ville.vainio@gmail.com>
2015-03-01 14:27:29 +0200

    Add final specs for ngbeerlist page

--- spec/features/ngbeerlist_spec.rb
+++ spec/features/ngbeerlist_spec.rb
@@ -13,9 +13,9 @@ describe 'ngbeerlist page' do
     @brewery1 = FactoryGirl.create(:brewery, name: 'Koff')
     @brewery2 = FactoryGirl.create(:brewery, name: 'Schlenkerla')
     @brewery3 = FactoryGirl.create(:brewery, name: 'Ayinger')
-    @style1 = Style.create name: 'Lager'
-    @style2 = Style.create name: 'Rauchbier'
-    @style3 = Style.create name: 'Weizen'
+    @style1 = Style.create name: 'Lager', description: 'Description for lager'
+    @style2 = Style.create name: 'Rauchbier', description: 'Description for rauchbier'
+    @style3 = Style.create name: 'Weizen', description: 'Description for weizen'
     @beer1 = FactoryGirl.create(:beer, name: 'Nikolai', brewery: @brewery1, style: @style1)
     @beer2 = FactoryGirl.create(:beer, name: 'Fastenbier', brewery: @brewery2, style: @style2)
     @beer3 = FactoryGirl.create(:beer, name: 'Lechte Weisse', brewery: @brewery3, style: @style3)
@@ -31,7 +31,33 @@ describe 'ngbeerlist page' do
 
   it 'shows one known beer', js: true do
     visit ngbeerlist_path
-    find('table').find('tr:nth-child(2)')
+    find('table').find('tbody > tr:nth-child(1)')
     expect(page).to have_content 'Nikolai'
+  end
+
+  it 'has beers sorted alphabetically by name', js: true do
+    visit ngbeerlist_path
+    first = find('table').find('tbody > tr:nth-child(1)')
+    last = find('table').find('tbody > tr:nth-child(3)')
+    expect(first).to have_content 'Fastenbier'
+    expect(last).to have_content 'Nikolai'
+  end
+
+  it 'has beers sorted alphabetically by style', js: true do
+    visit ngbeerlist_path
+    find('table > thead > tr > th:nth-child(2) > a').click
+    first = find('table').find('tbody > tr:nth-child(1)')
+    last = find('table').find('tbody > tr:nth-child(3)')
+    expect(first).to have_content 'Lager'
+    expect(last).to have_content 'Weizen'
+  end
+
+  it 'has beers sorted alphabetically by brewery', js: true do
+    visit ngbeerlist_path
+    find('table > thead > tr > th:nth-child(3) > a').click
+    first = find('table').find('tbody > tr:nth-child(1)')
+    last = find('table').find('tbody > tr:nth-child(3)')
+    expect(first).to have_content 'Ayinger'
+    expect(last).to have_content 'Schlenkerla'
   end
 end
