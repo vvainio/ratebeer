7d91791bb1d2b300fe8f020941e489b1fad93ab0
Ville Vainio <ville.vainio@gmail.com>
2015-03-01 18:37:40 +0200

    Include breweries and styles when fetching all beers (eager loading)

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -10,7 +10,7 @@ class BeersController < ApplicationController
   # GET /beers
   # GET /beers.json
   def index
-    @beers = Beer.all
+    @beers = Beer.includes(:brewery, :style).all
 
     order = params[:order] || 'name'
 
