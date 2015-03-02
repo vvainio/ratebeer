a75ab45c32ed6345cb29a7ee926a6768a6b9c4fc
Ville Vainio <ville.vainio@gmail.com>
2015-03-02 22:34:41 +0200

    Fix migration to work on Postgres

--- db/migrate/20150301124748_add_confirmed_to_membership.rb
+++ db/migrate/20150301124748_add_confirmed_to_membership.rb
@@ -2,8 +2,10 @@ class AddConfirmedToMembership < ActiveRecord::Migration
   def change
     add_column :memberships, :confirmed, :boolean, null: false, default: false
 
+    Membership.reset_column_information
+
     Membership.all.each do |m|
-      m.update_attribute(:confirmed, true)
+      m.confirmed = true
       m.save
     end
   end
